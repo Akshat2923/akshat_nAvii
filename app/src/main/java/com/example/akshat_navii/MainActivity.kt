1529af1a115096f7abe1c0a9bd5861a7834f21e8
Akshat Saladi <akshat.saladi@gmail.com>
2023-10-28 17:03:19 -0400

    new views11

--- app/src/main/java/com/example/akshat_navii/MainActivity.kt
+++ app/src/main/java/com/example/akshat_navii/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val transaction = supportFragmentManager.beginTransaction()
+
+            //transaction.repl
+        }
     }
 }
