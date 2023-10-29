ac788508e7181b8c7278ec4092f3873651d03de9
jackiexboadi <boadi.7@osu.edu>
2023-10-28 20:29:14 -0400

    Merge remote-tracking branch 'origin/jackie' into jackie
    # Conflicts:
    #	app/src/main/res/layout/fragment_homescreen.xml

--- app/src/main/java/com/example/akshat_navii/MainActivity.kt
+++ app/src/main/java/com/example/akshat_navii/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.akshat_navii
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
--- app/src/main/java/com/example/akshat_navii/homescreen.kt
+++ app/src/main/java/com/example/akshat_navii/homescreen.kt
@@ -1,59 +1,50 @@
 package com.example.akshat_navii
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [homescreen.newInstance] factory method to
- * create an instance of this fragment.
- */
 class homescreen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+private val startActivityForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result: ActivityResult ->
+    if (result.resultCode == Activity.RESULT_OK) {
+        val imageBitmap = result.data?.extras?.get("data") as Bitmap
     }
-
+}
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_homescreen, container, false)
+        val view = inflater.inflate(R.layout.fragment_homescreen, container, false)
+
+        val button: ImageButton = view.findViewById(R.id.button)
+        button.setOnClickListener {
+            // Code to open the camera
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult.launch(intent)
+        }
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment homescreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            homescreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val REQUEST_IMAGE_CAPTURE = 1
     }
-}
+    }
+
+
+
+
