1721da4d219c20375a488b026af3a584842279bc
Akshat Saladi <akshat.saladi@gmail.com>
2023-10-28 20:23:32 -0400

    first flow

--- app/src/main/java/com/example/akshat_navii/MainActivity.kt
+++ app/src/main/java/com/example/akshat_navii/MainActivity.kt
@@ -2,18 +2,33 @@ package com.example.akshat_navii
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.akshat_navii.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{
-            val transaction = supportFragmentManager.beginTransaction()
-
-            //transaction.repl
+        binding.button1.setOnClickListener{
+            replaceFragment(homescreen())
         }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mainLayout, fragment)
+        fragmentTransaction.addToBackStack(null)
+
+        fragmentTransaction.commit()
     }
 }
